d686506d471fbc4332362d856c52d53c73a281ff
Ethan Clayburn <emclayburn@gmail.com>
2025-11-16 02:57:31 -0700

    changed the layout

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from goalie_leaders import leaderboard_page
 from goalie_model import model_page
+from back_to_back import back_to_back_page
+
 
 pages = {
-    "🏒 Back-to-Back Team Analysis": None,   # existing page
+    "🏒 Back-to-Back Team Analysis": back_to_back_page,
     "🥅 Goalie Leaderboard": leaderboard_page,
     "🤖 Predictive Model": model_page
 }
 
 choice = st.sidebar.radio("Navigation", list(pages.keys()))
 
-if pages[choice] is not None:
-    pages[choice]()
+
+pages[choice]()
